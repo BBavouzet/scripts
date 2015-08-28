e318a866c540e578ebcfb94058f0df09f5004af4
TristeroEmpire <diogene.le.cynique@gmail.com>
2015-08-28 16:03:05 +0200

    Modifications majeures
    updateAtom.sh :
     - vérification de la présence de curl
     - pour plus de clarté : les couleurs ont été placées dans des variables
     - utilisation de apt et non de apt-get pour vérifier la présence de curl
     - création de la fonction main : fonction première du script
    
    mouveaux fichiers :
     - déplacements de temps.py et temps.sh pour une meilleure gestion des
       projets

---
+++ scripts-uptime/temps.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import re
+import time
+import sys
+
+
+def temps():
+    cpu = open("/proc/uptime", 'r')
+    # debug :
+    # cpu = open("uptime.txt", "r")
+    val = cpu.readline()
+    cpu.close()
+    rslt = re.compile("^\d+.\d{2}")
+    val2 = float(rslt.match(val).group())
+    t = time.gmtime(val2)
+    sys.stdout.write("Le système fonctionne depuis %d jour(s), %d heure(s) et %d minute(s)\
+    " % (t[2]-1, t[3], t[4]))
+
+temps()
--- updateAtom.sh
+++ updateAtom.sh
@@ -2,52 +2,73 @@
 
 #A éditer selon les versions installées : cf github pour les différents noms et extensions existants
 atom='atom-amd64.deb'
+dl=$(echo ~/Téléchargements)
+
+# couleurs
+vert="\e[1;92m"
+rouge="\e[1;91m"
+bleu="\e[1;96m"
+fin="\e[0m"
 
 #récupération du package 64bits avec wget en mode non-silencieux
 recup() {
-    echo -e "\e[1;92mRécupération avec wget...\e[0m"
-    cd ~/Téléchargements/
+    echo -e $bleu"Récupération avec wget...$fin"
+    cd $dl
     wget $1
-    echo -e "\e[1;96mFin du téléchargement.\e[0m"
+    echo -e $bleu"Fin du téléchargement.$fin"
 }
 
 #mise à jour d'Atom.
 updt() {
-    echo -e "\e[1;92mSuppression\e[0m de l'ancien package Deb d'Atom"
+    echo -e $vert"Suppression$fin de l'ancien package Deb d'Atom"
     sudo dpkg -r atom
     if [ -e $atom ]
     then
         sudo dpkg -i $atom
-        echo -e "\e[1;92mSuppression\e[0m du package sur le disque."
+        echo -e $vert"Suppression$fin du package sur le disque."
         rm $atom
         cd ~
         #vérification de la version du package
-        echo -e "\e[1;92mVérification\e[0m : Atom version : $(atom -v)"
+        echo -e $vert"Vérification$fin : Atom version : $(atom -v)"
     else
-        echo -e "\e[1;91mLe nom du fichier n'est pas le bon\e[Om : éditez le fichier"
+        echo -e $rouge"Le nom du fichier n'est pas le bon$fin : éditez le fichier"
     fi
 }
 
-rls=$(curl -s https://github.com/atom/atom/releases/latest)
-#debug :
-#rls="<html><body>You are being <a href=https://github.com/atom/atom/releases/tag/v1.0.7>redirected</a>.</body></html>"
-online=$(echo $rls | sed "s/^.*\(....[0-9]\).*$/\1/")
-toinstall="https://github.com/atom/atom/releases/download/v$online/atom-amd64.deb"
-install=$(atom -v)
+# fonction principale du programme
+main() {
+  rls=$(curl -s https://github.com/atom/atom/releases/latest)
+  #debug :
+  #rls="<html><body>You are being <a href=https://github.com/atom/atom/releases/tag/v1.0.7>redirected</a>.</body></html>"
+  online=$(echo $rls | sed "s/^.*\(....[0-9]\).*$/\1/")
+  toinstall="https://github.com/atom/atom/releases/download/v$online/atom-amd64.deb"
+  install=$(atom -v)
 
-#Point d'entrée du script : vérification des versions en ligne et en local
-if [ "$online" != "$install" ]
-then
-    echo "Dernière version en ligne disponible : $online"
-    echo "Souhaitez-vous la mettre à jour ? [o|n]"
-    read rep
-    if [ $rep == "o" ] || [ $rep == "oui" ]
+  if [ "$online" != "$install" ]
     then
-        recup $toinstall
-        updt
-    else
-        echo -e "\e[1;31mAnnulation\e[0m de toute mise à jour."
-    fi
+      echo -e "Dernière version en ligne disponible : $online"
+      echo "Souhaitez-vous la mettre à jour ? [o|n]"
+      read rep
+      if [ $rep == "o" ] || [ $rep == "oui" ]
+        then
+          recup $toinstall
+          updt
+      else
+          echo -e $rouge"Annulation$fin de toute mise à jour."
+      fi
+      else
+        echo -e $vert"Pas de mise à jour disponible$fin : la version installée localement ($vert$install$fin) est la plus récente."
+  fi
+}
+
+# Point d'entrée du script
+# Attention à bien vérifier les variables de débuts de script
+checkCurl=$(apt list --installed "curl")
+# 8 est la longueur du mot retournées par apt (nouvelle version)
+# si la longueur est supérieure à 8 alors curl est bien installé
+if [[ $(echo ${#checkCurl}) -gt 8 ]]
+  then
+  main
 else
-    echo -e "\e[1;92mPas de mise à jour disponible\e[0m : la version installée localement (\e[1;92mv$install\e[0m) est la plus récente."
+  echo -e "Le logiciel curl est requis\n\tInstallation: sudo apt install curl"
 fi
